44d6da4fc1b50af1992dfa57ad9904e932e5dbce
Kareem <ksegizekov@gmail.com>
2024-03-09 13:17:32 -0800

    Initial Commit

---
+++ fetch take home test ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fetch take home test ios
+//
+//  Created by Kareem Segizekov on 3/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fetch take home test ios/fetch_take_home_test_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetch_take_home_test_iosApp.swift
+//  fetch take home test ios
+//
+//  Created by Kareem Segizekov on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct fetch_take_home_test_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
